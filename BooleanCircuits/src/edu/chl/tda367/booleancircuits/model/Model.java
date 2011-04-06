e71d95da771121671c85afe4326f573a8ba581bf
jenpan90@hotmail.com <jenpan90@hotmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-06 12:36:27 +0000

    Start of implementation in the model class.

--- BooleanCircuits/src/edu/chl/tda367/booleancircuits/model/Model.java
+++ BooleanCircuits/src/edu/chl/tda367/booleancircuits/model/Model.java
@@ -1,5 +1,42 @@
 package edu.chl.tda367.booleancircuits.model;
 
-public interface Model {
+import java.util.*;
 
+
+public final class Model {
+List<CircuitComponent> componentList;
+	public Model(){
+
+		List componentList = new ArrayList();
+	}
+	/**
+	 * adds a new component
+	 */
+	private void addComponent(){
+		//componentList.add(<CircuitComponent> component);
+	}
+	/**
+	 * 
+	 * @return a list of selected components
+	 */
+	private  List<CircuitComponent> getSelectedComponents(){
+		return null;
+		
+	}
+	
+	/**
+	 * 
+	 */
+	private void removeSelectedComponent(){
+		
+	}
+	
+	private void setSelectedComponent(Coordinate coordinate){
+		
+	}
+	
+	private void selectAll(){
+		
+	}
+	//TODO: private void setNewConnection(){}
 }
