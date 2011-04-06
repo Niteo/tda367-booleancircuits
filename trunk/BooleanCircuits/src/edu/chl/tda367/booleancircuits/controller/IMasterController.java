4bf91615c6f7c2517799c07b56dedebb0413559e
robelirobban@gmail.com <robelirobban@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-06 10:17:14 +0000

    Fixed a typo in the sources. Added comments for the interface IMasterController

--- trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/controller/IMasterController.java
+++ trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/controller/IMasterController.java
@@ -5,23 +5,90 @@ package edu.chl.tda367.booleancircuits.controller;
  * @author Kaufmann
  * Interface for the main controller in BooleanCircuits
  */
+
 public interface IMasterController {
-	// Workspaces
+	/* ##########################
+	 * #       WORKSPACES       #
+	 * ##########################
+	 * */
+
+	/**
+	 * Closes the currently active workspace.
+	 */
 	public abstract void closeActiveWorkspace();
+	
+	/**
+	 * Creates a new blank workspace.
+	 */
 	public abstract void newWorkspace();
+	
+	/**
+	 * Opens the specified workspace.
+	 * @param path Path to the workspace to open.
+	 */
 	public abstract void openWorkspace(String path);
-	public abstract void saveActiveWorkspace(String path);
+	
+	/**
+	 * Saves the active workspace.
+	 */
+	public abstract void saveActiveWorkspace();
+	
+	/**
+	 * Saves the active workspace as a component.
+	 */
 	public abstract void saveActiveWorskpaceAsComponent(String path);
+	
+	/**
+	 * Saves all workspaces.
+	 */
 	public abstract void saveAllWorkspaces();
 	
-	// Component
+	/* ##########################
+	 * #       COMPONENTS       #
+	 * ##########################
+	 * */
+	/**
+	 * Adds the currently selected component in the palette to the
+	 * specified coordinate in the active workspace.
+	 * @param x The x-coordinate to add to
+	 * @param y The y-coordinate to add to
+	 */
 	public abstract void addComponent(int x, int y);
+	
+	//public abstract void useComponent(Component);
+	// TODO: Above method
+	/**
+	 * Removes the currently selected component(s) in the active workspace.
+	 */
 	public abstract void removeSelectedComponents();
+	/**
+	 * Selects all components in the active workspace.
+	 */
 	public abstract void selectAllComponents();
+	/**
+	 * Selects the first occurance of a component in the given coordinate
+	 * in the active workspace
+	 * @param x The x-coordinate to select from
+	 * @param y The y-coordinate to select from
+	 */
 	public abstract void selectComponent(int x, int y);
 	
-	// CutCopyPaste
+	/* ##########################
+	 * #      CutCopyPaste      #
+	 * ##########################
+	 * */
+	/**
+	 * Copies the selected components in the active workspace to clipboard.
+	 */
 	public abstract void copySelectedComponents();
+	/**
+	 * Cut the selected components in the active workspace to clipboard.
+	 */
 	public abstract void cutSelectedComponents();
-	public abstract void pasteSelectedComponent();
+	/**
+	 * Pastes the selected components to the active workspace from the clipboard.
+	 * @param x The x-coordinate to paste to
+	 * @param y The y-coordinate to paste to
+	 */
+	public abstract void pasteSelectedComponent(int x, int y);
 }
