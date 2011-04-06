cdf927825e4fd463c6d4e6048aecf6cce198d4bf
anton.lin1@gmail.com <anton.lin1@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-06 09:44:23 +0000

    Palette classes implemented. Not Fully.

---
+++ BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/PaletteView.java
@@ -0,0 +1,15 @@
+package edu.chl.tda367.booleancircuits.view;
+
+
+import java.util.List;
+import javax.swing.*;
+
+public final class PaletteView extends JPanel {
+	
+	List<String> folderNames;
+	List<String> componentNames;
+	
+	public PaletteView(){
+		
+	}	
+}
