a53242756db63d45ce924f63fb88d01b6cb845af
niteo1337@gmail.com <niteo1337@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-06 08:37:39 +0000

    Menus added.

--- BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/View.java
+++ BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/View.java
@@ -26,6 +26,8 @@ public class View extends javax.swing.JFrame {
 	// <editor-fold defaultstate="collapsed" desc="Generated Code">
 	private void initComponents() {
 
+		representationButtonGroup = new javax.swing.ButtonGroup();
+		backgroundButtonGroup = new javax.swing.ButtonGroup();
 		verticalSplitPane = new javax.swing.JSplitPane();
 		horizontalSplitPane = new javax.swing.JSplitPane();
 		paletteContainerPanel = new javax.swing.JPanel();
@@ -33,10 +35,39 @@ public class View extends javax.swing.JFrame {
 		toolbarContainerPanel = new javax.swing.JPanel();
 		menuBar = new javax.swing.JMenuBar();
 		fileMenu = new javax.swing.JMenu();
+		newWorkspaceMenuItem = new javax.swing.JMenuItem();
+		openFileMenuItem = new javax.swing.JMenuItem();
+		fileSeparator1 = new javax.swing.JSeparator();
+		closeMenuItem = new javax.swing.JMenuItem();
+		fileSeparator2 = new javax.swing.JSeparator();
+		saveMenuItem = new javax.swing.JMenuItem();
+		saveAsMenuItem = new javax.swing.JMenuItem();
+		saveAllMenuItem = new javax.swing.JMenuItem();
+		fileSeparator3 = new javax.swing.JSeparator();
+		exitMenuItem = new javax.swing.JMenuItem();
 		editMenu = new javax.swing.JMenu();
+		undoMenuItem = new javax.swing.JMenuItem();
+		redoMenuItem = new javax.swing.JMenuItem();
+		editSeparator1 = new javax.swing.JSeparator();
+		cutMenuItem = new javax.swing.JMenuItem();
+		copyMenuItem = new javax.swing.JMenuItem();
+		pasteMenuItem = new javax.swing.JMenuItem();
+		deleteMenuItem = new javax.swing.JMenuItem();
+		selectMenuItem = new javax.swing.JMenuItem();
+		selectAllMenuItem = new javax.swing.JMenuItem();
 		viewMenu = new javax.swing.JMenu();
+		representationMenu = new javax.swing.JMenu();
+		iecStandardRadioButtonMenuItem = new javax.swing.JRadioButtonMenuItem();
+		usStandardRadioButtonMenuItem = new javax.swing.JRadioButtonMenuItem();
+		backgroundMenu = new javax.swing.JMenu();
+		blankRadioButtonMenuItem = new javax.swing.JRadioButtonMenuItem();
+		dotsRadioButtonMenuItem = new javax.swing.JRadioButtonMenuItem();
+		squaresRadioButtonMenuItem = new javax.swing.JRadioButtonMenuItem();
 		insertMenu = new javax.swing.JMenu();
+		componentMenuItem = new javax.swing.JMenuItem();
 		helpMenu = new javax.swing.JMenu();
+		helpMenuItem = new javax.swing.JMenuItem();
+		aboutMenuItem = new javax.swing.JMenuItem();
 
 		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -73,36 +104,143 @@ public class View extends javax.swing.JFrame {
 		horizontalSplitPane.setRightComponent(canvasContainerPanel);
 
 		verticalSplitPane.setBottomComponent(horizontalSplitPane);
-
-		javax.swing.GroupLayout toolbarContainerPanelLayout = new javax.swing.GroupLayout(
-				toolbarContainerPanel);
-		toolbarContainerPanel.setLayout(toolbarContainerPanelLayout);
-		toolbarContainerPanelLayout
-				.setHorizontalGroup(toolbarContainerPanelLayout
-						.createParallelGroup(
-								javax.swing.GroupLayout.Alignment.LEADING)
-						.addGap(0, 670, Short.MAX_VALUE));
-		toolbarContainerPanelLayout
-				.setVerticalGroup(toolbarContainerPanelLayout
-						.createParallelGroup(
-								javax.swing.GroupLayout.Alignment.LEADING)
-						.addGap(0, 100, Short.MAX_VALUE));
-
 		verticalSplitPane.setLeftComponent(toolbarContainerPanel);
 
 		fileMenu.setText("File");
+
+		newWorkspaceMenuItem.setText("New Workspace");
+		fileMenu.add(newWorkspaceMenuItem);
+
+		openFileMenuItem.setText("Open File...");
+		fileMenu.add(openFileMenuItem);
+		fileMenu.add(fileSeparator1);
+
+		closeMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke(
+				java.awt.event.KeyEvent.VK_W,
+				java.awt.event.InputEvent.CTRL_MASK));
+		closeMenuItem.setText("Close");
+		fileMenu.add(closeMenuItem);
+		fileMenu.add(fileSeparator2);
+
+		saveMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke(
+				java.awt.event.KeyEvent.VK_S,
+				java.awt.event.InputEvent.CTRL_MASK));
+		saveMenuItem.setText("Save");
+		fileMenu.add(saveMenuItem);
+
+		saveAsMenuItem.setText("Save As...");
+		fileMenu.add(saveAsMenuItem);
+
+		saveAllMenuItem.setText("Save All");
+		fileMenu.add(saveAllMenuItem);
+		fileMenu.add(fileSeparator3);
+
+		exitMenuItem.setText("Exit");
+		fileMenu.add(exitMenuItem);
+
 		menuBar.add(fileMenu);
 
 		editMenu.setText("Edit");
+
+		undoMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke(
+				java.awt.event.KeyEvent.VK_Z,
+				java.awt.event.InputEvent.CTRL_MASK));
+		undoMenuItem.setText("Undo");
+		editMenu.add(undoMenuItem);
+
+		redoMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke(
+				java.awt.event.KeyEvent.VK_Y,
+				java.awt.event.InputEvent.CTRL_MASK));
+		redoMenuItem.setText("Redo");
+		editMenu.add(redoMenuItem);
+		editMenu.add(editSeparator1);
+
+		cutMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke(
+				java.awt.event.KeyEvent.VK_X,
+				java.awt.event.InputEvent.CTRL_MASK));
+		cutMenuItem.setText("Cut");
+		editMenu.add(cutMenuItem);
+
+		copyMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke(
+				java.awt.event.KeyEvent.VK_C,
+				java.awt.event.InputEvent.CTRL_MASK));
+		copyMenuItem.setText("Copy");
+		editMenu.add(copyMenuItem);
+
+		pasteMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke(
+				java.awt.event.KeyEvent.VK_V,
+				java.awt.event.InputEvent.CTRL_MASK));
+		pasteMenuItem.setText("Paste");
+		editMenu.add(pasteMenuItem);
+
+		deleteMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke(
+				java.awt.event.KeyEvent.VK_DELETE, 0));
+		deleteMenuItem.setText("Delete");
+		editMenu.add(deleteMenuItem);
+
+		selectMenuItem.setText("Select");
+		editMenu.add(selectMenuItem);
+
+		selectAllMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke(
+				java.awt.event.KeyEvent.VK_A,
+				java.awt.event.InputEvent.CTRL_MASK));
+		selectAllMenuItem.setText("Select All");
+		editMenu.add(selectAllMenuItem);
+
 		menuBar.add(editMenu);
 
 		viewMenu.setText("View");
+
+		representationMenu.setText("Representation");
+
+		representationButtonGroup.add(iecStandardRadioButtonMenuItem);
+		iecStandardRadioButtonMenuItem.setSelected(true);
+		iecStandardRadioButtonMenuItem.setText("IEC Standard");
+		representationMenu.add(iecStandardRadioButtonMenuItem);
+
+		representationButtonGroup.add(usStandardRadioButtonMenuItem);
+		usStandardRadioButtonMenuItem.setSelected(true);
+		usStandardRadioButtonMenuItem.setText("US Standard");
+		representationMenu.add(usStandardRadioButtonMenuItem);
+
+		viewMenu.add(representationMenu);
+
+		backgroundMenu.setText("Background");
+
+		backgroundButtonGroup.add(blankRadioButtonMenuItem);
+		blankRadioButtonMenuItem.setSelected(true);
+		blankRadioButtonMenuItem.setText("Blank");
+		backgroundMenu.add(blankRadioButtonMenuItem);
+
+		backgroundButtonGroup.add(dotsRadioButtonMenuItem);
+		dotsRadioButtonMenuItem.setSelected(true);
+		dotsRadioButtonMenuItem.setText("Dots");
+		backgroundMenu.add(dotsRadioButtonMenuItem);
+
+		backgroundButtonGroup.add(squaresRadioButtonMenuItem);
+		squaresRadioButtonMenuItem.setSelected(true);
+		squaresRadioButtonMenuItem.setText("Squares");
+		backgroundMenu.add(squaresRadioButtonMenuItem);
+
+		viewMenu.add(backgroundMenu);
+
 		menuBar.add(viewMenu);
 
 		insertMenu.setText("Insert");
+
+		componentMenuItem.setText("Component...");
+		insertMenu.add(componentMenuItem);
+
 		menuBar.add(insertMenu);
 
 		helpMenu.setText("Help");
+
+		helpMenuItem.setText("Help");
+		helpMenu.add(helpMenuItem);
+
+		aboutMenuItem.setText("About");
+		helpMenu.add(aboutMenuItem);
+
 		menuBar.add(helpMenu);
 
 		setJMenuBar(menuBar);
@@ -136,15 +274,46 @@ public class View extends javax.swing.JFrame {
 
 	//GEN-BEGIN:variables
 	// Variables declaration - do not modify
+	private javax.swing.JMenuItem aboutMenuItem;
+	private javax.swing.ButtonGroup backgroundButtonGroup;
+	private javax.swing.JMenu backgroundMenu;
+	private javax.swing.JRadioButtonMenuItem blankRadioButtonMenuItem;
 	private javax.swing.JPanel canvasContainerPanel;
+	private javax.swing.JMenuItem closeMenuItem;
+	private javax.swing.JMenuItem componentMenuItem;
+	private javax.swing.JMenuItem copyMenuItem;
+	private javax.swing.JMenuItem cutMenuItem;
+	private javax.swing.JMenuItem deleteMenuItem;
+	private javax.swing.JRadioButtonMenuItem dotsRadioButtonMenuItem;
 	private javax.swing.JMenu editMenu;
+	private javax.swing.JSeparator editSeparator1;
+	private javax.swing.JMenuItem exitMenuItem;
 	private javax.swing.JMenu fileMenu;
+	private javax.swing.JSeparator fileSeparator1;
+	private javax.swing.JSeparator fileSeparator2;
+	private javax.swing.JSeparator fileSeparator3;
 	private javax.swing.JMenu helpMenu;
+	private javax.swing.JMenuItem helpMenuItem;
 	private javax.swing.JSplitPane horizontalSplitPane;
+	private javax.swing.JRadioButtonMenuItem iecStandardRadioButtonMenuItem;
 	private javax.swing.JMenu insertMenu;
 	private javax.swing.JMenuBar menuBar;
+	private javax.swing.JMenuItem newWorkspaceMenuItem;
+	private javax.swing.JMenuItem openFileMenuItem;
 	private javax.swing.JPanel paletteContainerPanel;
+	private javax.swing.JMenuItem pasteMenuItem;
+	private javax.swing.JMenuItem redoMenuItem;
+	private javax.swing.ButtonGroup representationButtonGroup;
+	private javax.swing.JMenu representationMenu;
+	private javax.swing.JMenuItem saveAllMenuItem;
+	private javax.swing.JMenuItem saveAsMenuItem;
+	private javax.swing.JMenuItem saveMenuItem;
+	private javax.swing.JMenuItem selectAllMenuItem;
+	private javax.swing.JMenuItem selectMenuItem;
+	private javax.swing.JRadioButtonMenuItem squaresRadioButtonMenuItem;
 	private javax.swing.JPanel toolbarContainerPanel;
+	private javax.swing.JMenuItem undoMenuItem;
+	private javax.swing.JRadioButtonMenuItem usStandardRadioButtonMenuItem;
 	private javax.swing.JSplitPane verticalSplitPane;
 	private javax.swing.JMenu viewMenu;
 	// End of variables declaration//GEN-END:variables
