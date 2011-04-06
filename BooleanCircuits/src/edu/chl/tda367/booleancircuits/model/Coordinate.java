d696722158d77a022ece102824391ccd1f3bbd48
robelirobban@gmail.com <robelirobban@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-06 11:08:50 +0000

    Added interfaces and a couple of packages.

---
+++ BooleanCircuits/src/edu/chl/tda367/booleancircuits/model/Coordinate.java
@@ -0,0 +1,46 @@
+package edu.chl.tda367.booleancircuits.model;
+
+//TODO: Use interface above this?
+/**
+ * Simple class representing a coordinate.
+ * @author Kaufmann
+ */
+public class Coordinate {
+	public int x;
+	public int y;
+	
+	/**
+	 * Constructor for Coordinate.
+	 * @param x position on the X-axis
+	 * @param y position on the Y-axis
+	 */
+	public Coordinate(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	/**
+	 * Returns the x-position of the coordinate
+	 * @return an integer representing the x-position
+	 */
+	public int getX(){
+		return x;
+	}
+	
+	/**
+	 * Returns the y-position of the coordinate
+	 * @return an integer representing the y-position
+	 */
+	public int getY(){
+		return y;
+	}
+	
+	/**
+	 * Clones the current coordinate.
+	 * @return a clone of the current coordinate.
+	 */
+	@Override
+	public Coordinate clone(){
+		return new Coordinate(x, y);
+	}
+}
---
+++ BooleanCircuits/src/edu/chl/tda367/booleancircuits/utilities/ComponentFactory.java
@@ -0,0 +1,7 @@
+package edu.chl.tda367.booleancircuits.utilities;
+
+public final class ComponentFactory {
+	private ComponentFactory(){		
+	}
+	// TODO: Implement!
+}
