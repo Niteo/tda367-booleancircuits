543c3700271c64151f82ceb1db5e3ef5825684ec
niteo1337@gmail.com <niteo1337@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-06 13:43:11 +0000

    Canvas now has a JPanel instead of a tabbed pane.

--- BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/Canvas.java
+++ BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/Canvas.java
@@ -1,16 +1,24 @@
 package edu.chl.tda367.booleancircuits.view;
 
+import java.awt.*;
 import javax.swing.*;
 
-
 public class Canvas {
-	
-	private JTabbedPane tabbedPane = new JTabbedPane();
 
-	/** Returns the JTabbedPane of the canvas*/
-	public JTabbedPane getTabbedPane() {
-		return tabbedPane;
+	private JPanel panel = new JPanel();
+
+	/** Returns an instance of Canvas */
+	public Canvas() {
+		panel.setLayout(new BorderLayout());
 	}
-	
-	
+
+	/**
+	 * Returns the panel of the canvas
+	 * 
+	 * @return canvas JPanel
+	 */
+	public JPanel getPanel() {
+		return panel;
+	}
+
 }
