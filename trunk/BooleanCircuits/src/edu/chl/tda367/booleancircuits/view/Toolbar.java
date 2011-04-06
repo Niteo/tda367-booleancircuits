805c76d81e40c98f28c15b1480b567924610ab88
jenpan90@hotmail.com <jenpan90@hotmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-06 10:11:44 +0000

    a getToolbarPanel in the Toolbar class.

--- trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/Toolbar.java
+++ trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/Toolbar.java
@@ -1,5 +1,11 @@
 package edu.chl.tda367.booleancircuits.view;
 
 public class Toolbar {
+	Toolbar toolBar = new Toolbar();
+
+	/** returns the Toolbar*/
+	public Toolbar getToolbarPanel() {
+		return toolBar;
+	}
 
 }
