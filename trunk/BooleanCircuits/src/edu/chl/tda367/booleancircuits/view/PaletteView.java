0f2316088a2bd168cd6734dfc28d68b7b387666b
anton.lin1@gmail.com <anton.lin1@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-07 13:22:20 +0000

    further implementation

--- trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/PaletteView.java
+++ trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/PaletteView.java
@@ -1,19 +1,47 @@
 package edu.chl.tda367.booleancircuits.view;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
 
 public final class PaletteView {
 
-	private final JPanel paletteView;
+	private final JScrollPane paletteView;
 	private final List<ComponentFolder> folderList;
+	private JTree componentTree;
 
 	// kolla JTree...
 	public PaletteView(List<ComponentFolder> folderList) {
 
 		this.folderList = folderList;
-		paletteView = new JPanel();
+		paletteView = new JScrollPane();
 
+		initPaletteTree();
+
+	}
+
+	private void initPaletteTree() {
+
+		DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode(
+				"I will be invisible");
+
+		List<DefaultMutableTreeNode> folderNodeList = new ArrayList<DefaultMutableTreeNode>();
+		for (ComponentFolder cf : folderList) {
+			DefaultMutableTreeNode temp = new DefaultMutableTreeNode(
+					cf.getName());
+
+			folderNodeList.add(temp);
+			rootNode.add(temp);
+		}
+
+		componentTree = new JTree(rootNode);
+		componentTree.setRootVisible(false);
+	}
+
+	public JScrollPane getView() {
+		return paletteView;
 	}
 }
