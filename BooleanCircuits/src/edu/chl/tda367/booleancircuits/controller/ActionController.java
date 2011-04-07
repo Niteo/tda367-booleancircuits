2a8b72ee4062ead16ae145b4a8ac9c32fc39c855
niteo1337@gmail.com <niteo1337@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-07 13:36:33 +0000

    Added an ActionController, and actionlisteners to the buttons and menu items. Also added getters for the toolbarPanel.

---
+++ BooleanCircuits/src/edu/chl/tda367/booleancircuits/controller/ActionController.java
@@ -0,0 +1,14 @@
+package edu.chl.tda367.booleancircuits.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ActionController implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Add action performed for all buttons
+		
+	}
+
+}
