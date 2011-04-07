9b68fe47aa31527ee958dc4b2325089875236015
niteo1337@gmail.com <niteo1337@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-07 14:19:58 +0000

    ActionController now has a master controller. All buttons(?) now have an actionlistener.

--- BooleanCircuits/src/edu/chl/tda367/booleancircuits/controller/ActionController.java
+++ BooleanCircuits/src/edu/chl/tda367/booleancircuits/controller/ActionController.java
@@ -3,12 +3,27 @@ package edu.chl.tda367.booleancircuits.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * A listener class to controll action events.
+ * @author Boel
+ *
+ */
 public class ActionController implements ActionListener {
 
+	private MasterController mc;
+
+	/**
+	 * Returns an instance of ActionController.
+	 * @param MasterController
+	 */
+	public ActionController(MasterController masterController) {
+		mc = masterController;
+	}
+
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent e) {
 		// TODO Add action performed for all buttons
-		
+		System.out.println(e.getSource().toString());
 	}
 
 }
