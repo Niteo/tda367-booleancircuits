4f8aee85ab053b80eaad77962ec68a861833023a
anton.lin1@gmail.com <anton.lin1@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-07 12:23:37 +0000

    Deleted "PaletteComponentButton" and "PaletteFolderButton". Further implemented ComponentFolder.

--- BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/PaletteView.java
+++ BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/PaletteView.java
@@ -1,47 +1,19 @@
 package edu.chl.tda367.booleancircuits.view;
 
-
-import java.util.ArrayList;
 import java.util.List;
-import javax.swing.*;
+
+import javax.swing.JPanel;
 
 public final class PaletteView {
-	
-	List<String> folderNames;
-	List<String> componentNames;
-	List<PaletteFolderButton> folderButtonList;
-	
-	JPanel paletteView;
-	
-	public PaletteView(){
-		
-		//Hårdkodning av folderNames tillsvidare
-		folderNames = new ArrayList<String>();
-		folderNames.add("Hallå");
-		folderNames.add("Hejdå");
-				
+
+	private final JPanel paletteView;
+	private final List<ComponentFolder> folderList;
+
+	// kolla JTree...
+	public PaletteView(List<ComponentFolder> folderList) {
+
+		this.folderList = folderList;
 		paletteView = new JPanel();
-		paletteView.setLayout(new BoxLayout(paletteView,BoxLayout.Y_AXIS));
-		
-		initFolderButtons();
-	}	
-	/**
-	 * Initiates all the folderButtons
-	 */
-	public void initFolderButtons(){
-		
-		folderButtonList = new ArrayList<PaletteFolderButton>();
-		
-		for(int i = 0; i < folderNames.size(); i++){
-			folderButtonList.add(new PaletteFolderButton(folderNames.get(i)));
-		}		
-	}
-	/**
-	 * Adds all folderButtons to palette
-	 */
-	public void addFolderButtons(){
-		for(int i = 0; i < folderButtonList.size(); i++){
-			paletteView.add(folderButtonList.get(i));
-		}
+
 	}
 }
