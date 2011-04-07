b6f580a7b0cd6a1a3dc653a6226abdd9b20d4bb1
jenpan90@hotmail.com <jenpan90@hotmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-07 14:17:19 +0000

    Fully implemented AND-gate

--- trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/model/components/AndGate.java
+++ trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/model/components/AndGate.java
@@ -7,8 +7,14 @@ import java.util.List;
  * @author Jennifer Represents an AND-gate Creates input and outputs and updates
  *         the output
  */
-public class AndGate extends AbstractCircuitGate {
+public final class AndGate extends AbstractCircuitGate {
 
+	/**
+	 * A new AND-gate is created depending on the number of inputs
+	 * 
+	 * @param Number
+	 *            of inputs to the AND-gate
+	 */
 	public AndGate(int noOfInputs) {
 		super.createInputs(noOfInputs);
 		super.createOutputs(1);
@@ -18,11 +24,15 @@ public class AndGate extends AbstractCircuitGate {
 	 * sets the output depending on the inputs to a AND-component
 	 */
 	@Override
-	public void updateOutput() {
+	protected void updateOutput() {
 		List<ComponentInput> input = super.getInputs();
 
-		super.setOutput(0, input.get(0).getInputValue()
-				&& input.get(1).getInputValue());
+		for (ComponentInput i : input) {
+			if (i.getInputValue() == false) {
+				super.setOutput(0, false);
+			}
+			super.setOutput(0, true);
+		}
 
 	}
 }
