190af17d811691f263d89d201437daa617bfd262
robelirobban@gmail.com <robelirobban@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-08 09:25:51 +0000

    Created some template classes for the remaining gates.

---
+++ trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/model/components/NandGate.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package edu.chl.tda367.booleancircuits.model.components;
+
+/**
+ * @author Kaufmann
+ *
+ */
+public final class NandGate extends AbstractCircuitGate {
+
+	/**
+	 * TODO: Enter javadocs here
+	 */
+	public NandGate(){
+		
+	}
+	
+	/**
+	 * TODO: Enter javadocs here
+	 */
+	@Override
+	protected void updateOutput() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/model/components/XorGate.java
@@ -0,0 +1,20 @@
+package edu.chl.tda367.booleancircuits.model.components;
+
+public final class XorGate extends AbstractCircuitGate {
+	/**
+	 * TODO: Write Javadoc
+	 */
+	public XorGate(){
+		
+	}
+	
+	/**
+	 * TODO:
+	 */
+	@Override
+	protected void updateOutput() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
