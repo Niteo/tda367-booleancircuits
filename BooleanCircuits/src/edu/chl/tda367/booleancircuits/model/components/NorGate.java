d28e354994b52b1cc4ecba59ed15e34b049e04f1
robelirobban@gmail.com <robelirobban@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-08 09:25:51 +0000

    Created some template classes for the remaining gates.

---
+++ BooleanCircuits/src/edu/chl/tda367/booleancircuits/model/components/NorGate.java
@@ -0,0 +1,20 @@
+package edu.chl.tda367.booleancircuits.model.components;
+
+public final class NorGate extends AbstractCircuitGate {
+
+	/**
+	 * TODO: Enter javadoc here
+	 */
+	public NorGate(){
+		
+	}
+	/**
+	 * TODO: Enter javadoc here
+	 */
+	@Override
+	protected void updateOutput() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ BooleanCircuits/src/edu/chl/tda367/booleancircuits/model/components/NxorGate.java
@@ -0,0 +1,21 @@
+package edu.chl.tda367.booleancircuits.model.components;
+
+import edu.chl.tda367.booleancircuits.model.components.*;
+
+public final class NxorGate extends AbstractCircuitGate {
+
+	/**
+	 * TODO: Javadoc
+	 */
+	public NxorGate(){
+		
+	}
+	/**
+	 * TODO: Javadoc
+	 */
+	@Override
+	protected void updateOutput() {
+		// TODO Auto-generated method stub
+		
+	}
+}
