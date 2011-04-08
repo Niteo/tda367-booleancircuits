8b4d2980903ffc1267d6eab2a0ad258cab312ca9
anton.lin1@gmail.com <anton.lin1@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-08 08:51:06 +0000

    asgsarg

--- trunk/BooleanCircuits/test/testPalette.java
+++ trunk/BooleanCircuits/test/testPalette.java
@@ -3,14 +3,14 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
-import edu.chl.tda367.booleancircuits.view.PaletteView;
+import edu.chl.tda367.booleancircuits.view.Palette;
 
 public class testPalette extends javax.swing.JPanel {
 
 	/**
 	 * Auto-generated main method to display this JPanel inside a new JFrame.
 	 */
-	private final PaletteView palette = new PaletteView();
+	private final Palette palette = new Palette();
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
