e6a83a840b39fd3a08275706db06805cc6c3f055
niteo1337@gmail.com <niteo1337@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-11 21:25:59 +0000

    Actions added to ActionController, most buttons and menuitems are now connected to their action. The icons and menutext disappear when running for some strange reason...!

--- trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/controller/ActionController.java
+++ trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/controller/ActionController.java
@@ -1,14 +1,15 @@
 package edu.chl.tda367.booleancircuits.controller;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 
 /**
- * A listener class to controll action events.
+ * A listener class to control action events.
  * @author Boel
  *
  */
-public class ActionController implements ActionListener {
+public class ActionController {
 
 	private MasterController mc;
 
@@ -20,10 +21,203 @@ public class ActionController implements ActionListener {
 		mc = masterController;
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Add action performed for all buttons
-		System.out.println(e.getSource().toString());
+	private Action newWorkspaceAction = new AbstractAction(){
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			mc.newWorkspace();
+		}
+		
+	};
+	
+	private Action closeActiveWorkspaceAction = new AbstractAction() {
+		
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			mc.closeActiveWorkspace();
+		}
+	};
+	
+	private Action openWorkspaceAction = new AbstractAction() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			//TODO add correct path
+			mc.openWorkspace("");
+		}
+	};
+	
+	private Action saveActiveWorkspaceAction = new AbstractAction() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			mc.saveActiveWorkspace();
+		}
+	};
+	
+	private Action saveActiveWorskpaceAsComponentAction = new AbstractAction() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			//TODO add correct path
+			mc.saveActiveWorskpaceAsComponent("");
+		}
+	};
+	
+	private Action saveAllWorkspacesAction = new AbstractAction() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			mc.saveAllWorkspaces();
+		}
+	};
+	
+	private Action copySelectedComponentsAction = new AbstractAction() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			mc.copySelectedComponents();
+		}
+	};
+	
+	private Action cutSelectedComponentsAction = new AbstractAction() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			mc.cutSelectedComponents();
+		}
+	};
+	
+	private Action pasteSelectedComponentAction = new AbstractAction() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			//TODO add correct coordinates
+			mc.pasteSelectedComponent(null);
+		}
+	};
+	
+	private Action undoAction = new AbstractAction() {
+		
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO undo
+		}
+	};
+	
+	private Action redoAction = new AbstractAction() {
+		
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO redo
+		}
+	};
+	
+	private Action closeAction = new AbstractAction() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.exit(0);
+		}
+	};
+	
+	private Action addComponentAction = new AbstractAction() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			//TODO get coords and component
+			mc.addComponent(null, null);
+		}
+	};
+	
+	private Action removeSelectedComponentsAction = new AbstractAction() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			mc.removeSelectedComponents();
+		}
+	};
+	
+	private Action selectAllComponentsAction = new AbstractAction() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			mc.selectAllComponents();
+		}
+	};
+	
+	private Action selectComponentAction = new AbstractAction() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO get the coords of the selected component
+			mc.selectComponent(null);
+		}
+	};
+
+	public Action getNewWorkspaceAction() {
+		return newWorkspaceAction;
+	}
+	
+	public Action getCloseActiveWorkspaceAction() {
+		return closeActiveWorkspaceAction;
 	}
 
+	public Action getOpenWorkspaceAction() {
+		return openWorkspaceAction;
+	}
+
+	public Action getSaveActiveWorkspaceAction() {
+		return saveActiveWorkspaceAction;
+	}
+
+	public Action getSaveActiveWorskpaceAsComponentAction() {
+		return saveActiveWorskpaceAsComponentAction;
+	}
+
+	public Action getSaveAllWorkspacesAction() {
+		return saveAllWorkspacesAction;
+	}
+
+	public Action getCopySelectedComponentsAction() {
+		return copySelectedComponentsAction;
+	}
+
+	public Action getCutSelectedComponentsAction() {
+		return cutSelectedComponentsAction;
+	}
+
+	public Action getPasteSelectedComponentAction() {
+		return pasteSelectedComponentAction;
+	}
+
+	public Action getUndoAction() {
+		return undoAction;
+	}
+
+	public Action getRedoAction() {
+		return redoAction;
+	}
+	
+	public Action getCloseAction() {
+		return closeAction;
+	}
+
+	public Action getAddComponentAction() {
+		return addComponentAction;
+	}
+
+	public Action getRemoveSelectedComponentsAction() {
+		return removeSelectedComponentsAction;
+	}
+
+	public Action getSelectAllComponentsAction() {
+		return selectAllComponentsAction;
+	}
+
+	public Action getSelectComponentAction() {
+		return selectComponentAction;
+	}
+	
+	
 }
