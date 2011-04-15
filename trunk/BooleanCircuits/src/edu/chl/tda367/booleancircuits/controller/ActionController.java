1053920e21344187d8496b229f0cce6e4f27790a
niteo1337@gmail.com <niteo1337@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-15 09:14:06 +0000

    Icons and menu items text is now being set AFTER the actions. Added some Javadoc to the ActionController class.

--- trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/controller/ActionController.java
+++ trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/controller/ActionController.java
@@ -3,11 +3,13 @@ package edu.chl.tda367.booleancircuits.controller;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.ImageIcon;
 
 /**
  * A class to control action events.
+ * 
  * @author Boel
- *
+ * 
  */
 public class ActionController {
 
@@ -15,139 +17,140 @@ public class ActionController {
 
 	/**
 	 * Returns an instance of ActionController.
+	 * 
 	 * @param MasterController
 	 */
 	public ActionController(MasterController masterController) {
 		mc = masterController;
 	}
 
-	private Action newWorkspaceAction = new AbstractAction(){
+	private Action newWorkspaceAction = new AbstractAction() {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			mc.newWorkspace();
 		}
-		
+
 	};
-	
+
 	private Action closeActiveWorkspaceAction = new AbstractAction() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			mc.closeActiveWorkspace();
 		}
 	};
-	
+
 	private Action openWorkspaceAction = new AbstractAction() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			//TODO add correct path
+			// TODO add correct path
 			mc.openWorkspace("");
 		}
 	};
-	
+
 	private Action saveActiveWorkspaceAction = new AbstractAction() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			mc.saveActiveWorkspace();
 		}
 	};
-	
+
 	private Action saveActiveWorskpaceAsComponentAction = new AbstractAction() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			//TODO add correct path
+			// TODO add correct path
 			mc.saveActiveWorskpaceAsComponent("");
 		}
 	};
-	
+
 	private Action saveAllWorkspacesAction = new AbstractAction() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			mc.saveAllWorkspaces();
 		}
 	};
-	
+
 	private Action copySelectedComponentsAction = new AbstractAction() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			mc.copySelectedComponents();
 		}
 	};
-	
+
 	private Action cutSelectedComponentsAction = new AbstractAction() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			mc.cutSelectedComponents();
 		}
 	};
-	
+
 	private Action pasteSelectedComponentAction = new AbstractAction() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			//TODO add correct coordinates
+			// TODO add correct coordinates
 			mc.pasteSelectedComponent(null);
 		}
 	};
-	
+
 	private Action undoAction = new AbstractAction() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			// TODO undo
 		}
 	};
-	
+
 	private Action redoAction = new AbstractAction() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			// TODO redo
 		}
 	};
-	
-	private Action closeAction = new AbstractAction() {
-		
+
+	private Action exitAction = new AbstractAction() {
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			System.exit(0);
 		}
 	};
-	
+
 	private Action addComponentAction = new AbstractAction() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			//TODO get coords and component
+			// TODO get coords and component
 			mc.addComponent(null, null);
 		}
 	};
-	
+
 	private Action removeSelectedComponentsAction = new AbstractAction() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			mc.removeSelectedComponents();
 		}
 	};
-	
+
 	private Action selectAllComponentsAction = new AbstractAction() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			mc.selectAllComponents();
 		}
 	};
-	
+
 	private Action selectComponentAction = new AbstractAction() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO get the coords of the selected component
@@ -155,69 +158,132 @@ public class ActionController {
 		}
 	};
 
+	/**
+	 * Returns the newWorkspace action
+	 * @return Action
+	 */
 	public Action getNewWorkspaceAction() {
 		return newWorkspaceAction;
 	}
-	
+
+	/**
+	 * Returns the closeActiveWorkspace action
+	 * @return Action
+	 */
 	public Action getCloseActiveWorkspaceAction() {
 		return closeActiveWorkspaceAction;
 	}
 
+	/**
+	 * Returns the openWorkspace action
+	 * @return Action
+	 */
 	public Action getOpenWorkspaceAction() {
 		return openWorkspaceAction;
 	}
 
+	/**
+	 * Returns the saveActiveWorkspace action
+	 * @return Action
+	 */
 	public Action getSaveActiveWorkspaceAction() {
 		return saveActiveWorkspaceAction;
 	}
 
+	/**
+	 * Returns the saveActiveWorkspaceAsComponent action
+	 * @return Action
+	 */
 	public Action getSaveActiveWorskpaceAsComponentAction() {
 		return saveActiveWorskpaceAsComponentAction;
 	}
 
+	/**
+	 * Returns the saveAllWorkspaces action
+	 * @return Action
+	 */
 	public Action getSaveAllWorkspacesAction() {
 		return saveAllWorkspacesAction;
 	}
 
+	/**
+	 * Returns the CopySelectedComponents action
+	 * @return Action
+	 */
 	public Action getCopySelectedComponentsAction() {
 		return copySelectedComponentsAction;
 	}
 
+	/**
+	 * Returns the cutSelectedComponents action
+	 * @return Action
+	 */
 	public Action getCutSelectedComponentsAction() {
 		return cutSelectedComponentsAction;
 	}
 
+	/**
+	 * Returns the pasteSelectedComponent action
+	 * @return Action
+	 */
 	public Action getPasteSelectedComponentAction() {
 		return pasteSelectedComponentAction;
 	}
 
+	/**
+	 * Returns the undo action
+	 * @return Action
+	 */
 	public Action getUndoAction() {
 		return undoAction;
 	}
 
+	/**
+	 * Returns the redo action
+	 * @return Action
+	 */
 	public Action getRedoAction() {
 		return redoAction;
 	}
-	
-	public Action getCloseAction() {
-		return closeAction;
+
+	/**
+	 * Returns the exit action
+	 * @return Action
+	 */
+	public Action getExitAction() {
+		return exitAction;
 	}
 
+	/**
+	 * Returns the addComponent action
+	 * @return Action
+	 */
 	public Action getAddComponentAction() {
 		return addComponentAction;
 	}
 
+	/**
+	 * Returns the removeSelectedComponents action
+	 * @return Action
+	 */
 	public Action getRemoveSelectedComponentsAction() {
 		return removeSelectedComponentsAction;
 	}
 
+	/**
+	 * Returns the selectAllComponents action
+	 * @return Action
+	 */
 	public Action getSelectAllComponentsAction() {
 		return selectAllComponentsAction;
 	}
 
+	/**
+	 * Returns the selectComponent action
+	 * @return Action
+	 */
 	public Action getSelectComponentAction() {
 		return selectComponentAction;
 	}
-	
-	
+
 }
