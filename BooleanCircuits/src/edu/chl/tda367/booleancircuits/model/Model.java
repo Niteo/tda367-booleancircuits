b6b5c54aeaa5fef8afb274e9fcb2856a4e7df1cf
niteo1337@gmail.com <niteo1337@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-27 10:58:38 +0000

    Model unlocked.

--- BooleanCircuits/src/edu/chl/tda367/booleancircuits/model/Model.java
+++ BooleanCircuits/src/edu/chl/tda367/booleancircuits/model/Model.java
@@ -10,17 +10,21 @@ import edu.chl.tda367.booleancircuits.model.components.AbstractCircuitGate;
 public final class Model {
 
 	private String name;
+	private int id;
 	private final List<AbstractCircuitGate> componentList;
 	private final List<AbstractCircuitGate> selectedComponentList;
 
 	/**
 	 * Returns an instance of Model
-	 * @param name the name of the model
+	 * 
+	 * @param name
+	 *            the name of the model
 	 */
-	public Model(String name) {
+	public Model(String name, int id) {
 		componentList = new ArrayList<AbstractCircuitGate>();
 		selectedComponentList = new ArrayList<AbstractCircuitGate>();
 		this.name = name;
+		this.id = id;
 	}
 
 	/**
@@ -41,21 +45,25 @@ public final class Model {
 	 */
 	public List<AbstractCircuitGate> getSelectedComponents() {
 		return selectedComponentList;
-	/*	for(int i = 0; i<=selectedComponentList.size();i++){
-			List<AbstractCircuitGate> selectedList = new ArrayList<AbstractCircuitGate>();
-			selectedList = ((List<AbstractCircuitGate>) selectedComponentList).
-			
-		}
-		return null;*/
-		//TODO: clone selectedComponentList
+		/*
+		 * for(int i = 0; i<=selectedComponentList.size();i++){
+		 * List<AbstractCircuitGate> selectedList = new
+		 * ArrayList<AbstractCircuitGate>(); selectedList =
+		 * ((List<AbstractCircuitGate>) selectedComponentList).
+		 * 
+		 * } return null;
+		 */
+		// TODO: clone selectedComponentList
 	}
+
 	/**
 	 * Returns a reference to the list of components
+	 * 
 	 * @return a reference to the list of components
 	 */
 	public List<AbstractCircuitGate> getComponents() {
 		return componentList;
-		//TODO: clone componentList
+		// TODO: clone componentList
 	}
 
 	/**
@@ -93,37 +101,37 @@ public final class Model {
 		// Sort our components in groups of update tiers.
 		int loopCount = 1;
 		boolean endOfLoop = true;
-		do{
+		do {
 			List<AbstractCircuitGate> tempList = new LinkedList<AbstractCircuitGate>();
 			endOfLoop = true;
 			for (AbstractCircuitGate i : componentList) {
 				if (i.getComponentTier() == loopCount) {
 					tempList.add(i);
-					endOfLoop = false; 
+					endOfLoop = false;
 				}
 			}
 		} while (!endOfLoop);
 
 		// Update each tier individually
-		for(List<AbstractCircuitGate> l : groupList){
+		for (List<AbstractCircuitGate> l : groupList) {
 			List<AbstractCircuitGate> cloneList = new ArrayList<AbstractCircuitGate>();
-			for(AbstractCircuitGate g : l){
+			for (AbstractCircuitGate g : l) {
 				AbstractCircuitGate temp = g.clone();
 				temp.update();
 				cloneList.add(temp);
 			}
-			
-			for(int i = 0; i < cloneList.size(); i++){
+
+			for (int i = 0; i < cloneList.size(); i++) {
 				l.get(i).overwriteGate(cloneList.get(i));
 			}
 		}
 	}
-	
+
 	/**
 	 * Returns the name of the workspace
 	 */
 	@Override
-	public String toString(){
+	public String toString() {
 		return name;
 	}
 }
