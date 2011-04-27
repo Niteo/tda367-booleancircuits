eff235ae30ff6b7bb2a13857f6cf9ee8f9f88cd7
anton.lin1@gmail.com <anton.lin1@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-27 12:21:43 +0000

    Changes in constructor

--- trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/Canvas.java
+++ trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/Canvas.java
@@ -2,14 +2,11 @@ package edu.chl.tda367.booleancircuits.view;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.swing.JPanel;
 
 import edu.chl.tda367.booleancircuits.model.Model;
 import edu.chl.tda367.booleancircuits.model.components.AbstractCircuitGate;
-import edu.chl.tda367.booleancircuits.model.components.NandGate;
 
 /**
  * A class where the components are drawn.
@@ -21,40 +18,23 @@ public class Canvas {
 
 	private JPanel panel;
 	private Model model;
-	// Just for test
-	private List<AbstractCircuitGate> testList;
 
-	// Testkonstruktor
 	public Canvas() {
 
-		testList = new ArrayList<AbstractCircuitGate>();
-		testList.add(new NandGate(0));
-
 		panel = new JPanel() {
 			@Override
 			public void paint(Graphics g) {
 				// TODO implement paint
 				g.setColor(Color.black);
-				for (AbstractCircuitGate circuitGate : testList) {
-					g.drawRect(50, 70, 80, 70);
-					g.drawString(circuitGate.toString(), 70, 110);
-				}
-			}
-		};
-		panel.setBackground(Color.WHITE);
-	}
-
-	public Canvas(final Model model) {
-
-		this.model = model;
-		panel = new JPanel() {
-			@Override
-			public void paint(Graphics g) {
-				// TODO implement paint
-				g.setColor(Color.black);
-				for (AbstractCircuitGate circuitGate : testList) {
-					g.drawRect(50, 70, 80, 70);
-					g.drawString(circuitGate.toString(), 50, 60);
+				if (model != null) {
+					for (AbstractCircuitGate circuitGate : model
+							.getComponents()) {
+						g.drawRect(circuitGate.getPosition().x,
+								circuitGate.getPosition().y, 80, 70);
+						g.drawString(circuitGate.toString(),
+								circuitGate.getPosition().x,
+								circuitGate.getPosition().y);
+					}
 				}
 			}
 		};
@@ -68,5 +48,9 @@ public class Canvas {
 	 */
 	public JPanel getCanvas() {
 		return panel;
+	}
+
+	public void setModel(Model model) {
+		this.model = model;
 	}
 }
