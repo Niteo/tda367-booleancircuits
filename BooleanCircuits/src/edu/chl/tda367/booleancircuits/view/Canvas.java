e97d68a009c9bb9808b50f627f3cfb017fe16a3c
anton.lin1@gmail.com <anton.lin1@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-04-27 12:02:01 +0000

    Created test constructor in canvas and TestCanvas.class

--- BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/Canvas.java
+++ BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/Canvas.java
@@ -2,10 +2,14 @@ package edu.chl.tda367.booleancircuits.view;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JPanel;
 
 import edu.chl.tda367.booleancircuits.model.Model;
+import edu.chl.tda367.booleancircuits.model.components.AbstractCircuitGate;
+import edu.chl.tda367.booleancircuits.model.components.NandGate;
 
 /**
  * A class where the components are drawn.
@@ -17,18 +21,44 @@ public class Canvas {
 
 	private JPanel panel;
 	private Model model;
+	// Just for test
+	private List<AbstractCircuitGate> testList;
 
-	public Canvas(Model model) {
+	// Testkonstruktor
+	public Canvas() {
+
+		testList = new ArrayList<AbstractCircuitGate>();
+		testList.add(new NandGate(0));
 
 		panel = new JPanel() {
 			@Override
 			public void paint(Graphics g) {
 				// TODO implement paint
+				g.setColor(Color.black);
+				for (AbstractCircuitGate circuitGate : testList) {
+					g.drawRect(50, 70, 80, 70);
+					g.drawString(circuitGate.toString(), 70, 110);
+				}
 			}
 		};
 		panel.setBackground(Color.WHITE);
+	}
+
+	public Canvas(final Model model) {
 
 		this.model = model;
+		panel = new JPanel() {
+			@Override
+			public void paint(Graphics g) {
+				// TODO implement paint
+				g.setColor(Color.black);
+				for (AbstractCircuitGate circuitGate : testList) {
+					g.drawRect(50, 70, 80, 70);
+					g.drawString(circuitGate.toString(), 50, 60);
+				}
+			}
+		};
+		panel.setBackground(Color.WHITE);
 	}
 
 	/**
