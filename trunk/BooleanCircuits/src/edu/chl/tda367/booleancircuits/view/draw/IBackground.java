2585a3f07d65f0a456e8d50a10a5924a0b0db2e1
niteo1337@gmail.com <niteo1337@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-05-02 08:44:28 +0000

    Background classes added. Offset in IDraw and IBackground changed to Point.

--- trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/draw/IBackground.java
+++ trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/draw/IBackground.java
@@ -1,11 +1,13 @@
 package edu.chl.tda367.booleancircuits.view.draw;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Point;
 
 public interface IBackground {
 /**
  * Draws the background
  * @param g Graphics
  */
-	public void draw(Graphics g);
+	public void draw(Graphics g, Point offset, Dimension canvasSize);
 }
---
+++ trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/draw/SquaredBackground.java
@@ -0,0 +1,20 @@
+package edu.chl.tda367.booleancircuits.view.draw;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
+
+/**
+ * A class to draw a squared background.
+ * @author Boel
+ *
+ */
+public class SquaredBackground implements IBackground {
+
+	@Override
+	public void draw(Graphics g, Point offset, Dimension canvasSize) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
