55622163539dd9d43cb8ea4c40da12a7eb7f3b73
niteo1337@gmail.com <niteo1337@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-05-02 10:38:38 +0000

    Dotted background now working! SquaredBackground is now called GridBackground.

--- BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/draw/DottedBackground.java
+++ BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/draw/DottedBackground.java
@@ -4,17 +4,30 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Point;
 
+import edu.chl.tda367.booleancircuits.utilities.Constants;
+
 /**
  * A class to draw a dotted background.
+ * 
  * @author Boel
- *
+ * 
  */
-public class DottedBackground implements IBackground{
+public class DottedBackground implements IBackground {
 
 	@Override
 	public void draw(Graphics g, Point offset, Dimension canvasSize) {
-		// TODO Auto-generated method stub
-		
+		// TODO fix offset!
+
+		int radius= (int)(Constants.canvasDotDiameter*0.5);
+		for (int x = 0; x < canvasSize.width
+				* Constants.canvasBackgroundLineDistance; x += Constants.canvasBackgroundLineDistance) {
+			for (int y = 0; y < canvasSize.height
+					* Constants.canvasBackgroundLineDistance; y += Constants.canvasBackgroundLineDistance) {
+				g.drawRect(x-radius, y-radius, Constants.canvasDotDiameter,
+						Constants.canvasDotDiameter);
+			}
+		}
+
 	}
 
 }
