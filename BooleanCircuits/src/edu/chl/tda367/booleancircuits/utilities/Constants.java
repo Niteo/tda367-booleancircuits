a11ffa066a0b93bf3744a9ed38ddda16ca5095f5
robelirobban@gmail.com <robelirobban@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-05-02 08:58:38 +0000

    Constants added! :) NO MAGICAL STUFFS

---
+++ BooleanCircuits/src/edu/chl/tda367/booleancircuits/utilities/Constants.java
@@ -0,0 +1,6 @@
+package edu.chl.tda367.booleancircuits.utilities;
+
+public class Constants {
+	public static final int canvasBackgroundLineDistance = 32;
+	public static final int componentSize = 64;
+}
