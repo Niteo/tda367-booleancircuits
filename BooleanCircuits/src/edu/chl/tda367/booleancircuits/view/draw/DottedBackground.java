7120496c91dcd91b7e7674cf0dcd42ac2e882cdd
robelirobban@gmail.com <robelirobban@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-05-04 12:46:54 +0000

    Fixed offset for backgrounds! :D YAAAY!

--- BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/draw/DottedBackground.java
+++ BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/draw/DottedBackground.java
@@ -16,14 +16,13 @@ public class DottedBackground implements IBackground {
 
 	@Override
 	public void draw(Graphics g, Point offset, Dimension canvasSize) {
-		// TODO fix offset!
 
 		int radius= (int)(Constants.canvasDotDiameter*0.5);
 		for (int x = 0; x < canvasSize.width
 				* Constants.canvasBackgroundLineDistance; x += Constants.canvasBackgroundLineDistance) {
 			for (int y = 0; y < canvasSize.height
 					* Constants.canvasBackgroundLineDistance; y += Constants.canvasBackgroundLineDistance) {
-				g.drawRect(x-radius, y-radius, Constants.canvasDotDiameter,
+				g.drawRect((x-radius) + (int)offset.getX() * -1, y-radius  + (int)offset.getY() * -1, Constants.canvasDotDiameter,
 						Constants.canvasDotDiameter);
 			}
 		}
