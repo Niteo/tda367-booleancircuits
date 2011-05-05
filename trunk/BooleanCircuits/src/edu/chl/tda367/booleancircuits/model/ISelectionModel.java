f7c80ef6a6a3f78d08badfd43f37f2f72a70c14a
niteo1337@gmail.com <niteo1337@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-05-05 11:27:56 +0000

    SelectionModel class added, including an interface.

---
+++ trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/model/ISelectionModel.java
@@ -0,0 +1,44 @@
+package edu.chl.tda367.booleancircuits.model;
+
+import java.util.Collection;
+
+import edu.chl.tda367.booleancircuits.model.components.implementation.AbstractCircuitGate;
+
+/**
+ * An interface to describe a selection model.
+ * 
+ * @author Boel
+ * 
+ */
+public interface ISelectionModel {
+
+	/**
+	 * Selects all components in the collection.
+	 * 
+	 * @param c
+	 *            Collection
+	 */
+	public void selectAllComponents(Collection<AbstractCircuitGate> c);
+
+	/**
+	 * Selects the given component in the active workspace.
+	 * 
+	 * @param g
+	 *            AbstractCircuitGate the gate to select
+	 */
+	public void selectComponent(AbstractCircuitGate g);
+
+	/**
+	 * Determinates if a component is currently selected.
+	 * 
+	 * @param g
+	 *            AbstractCircuitGate
+	 * @return boolean
+	 */
+	public boolean isSelectedComponent(AbstractCircuitGate g);
+
+	/**
+	 * Removes all elements that are unused.
+	 */
+	public void removeUnusedElements();
+}
---
+++ trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/model/implementation/SelectionModel.java
@@ -0,0 +1,50 @@
+package edu.chl.tda367.booleancircuits.model.implementation;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import edu.chl.tda367.booleancircuits.model.ISelectionModel;
+import edu.chl.tda367.booleancircuits.model.components.implementation.AbstractCircuitGate;
+
+/**
+ * A class that manages what components are selected.
+ * 
+ * @author Boel
+ * 
+ */
+public class SelectionModel implements ISelectionModel {
+
+	private final List<AbstractCircuitGate> selectedComponentList;
+
+	public SelectionModel() {
+		selectedComponentList = new LinkedList<AbstractCircuitGate>();
+	}
+
+	@Override
+	public void selectAllComponents(Collection<AbstractCircuitGate> c) {
+		for (AbstractCircuitGate gate : c) {
+			selectedComponentList.add(gate);
+		}
+	}
+
+	@Override
+	public void selectComponent(AbstractCircuitGate g) {
+		selectedComponentList.add(g);
+	}
+
+	@Override
+	public boolean isSelectedComponent(AbstractCircuitGate g) {
+		if (selectedComponentList.contains(g)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public void removeUnusedElements() {
+		selectedComponentList.remove(null);
+	}
+
+}
