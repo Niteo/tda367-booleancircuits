5c846a2e20f3f3a6cfcd5d9f1622fb6245946a86
niteo1337@gmail.com <niteo1337@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-05-06 10:35:53 +0000

    Pop-up menu grayed out if the component doesn't have inputs or outputs.

--- trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/implementation/CanvasPopup.java
+++ trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/implementation/CanvasPopup.java
@@ -1,5 +1,7 @@
 package edu.chl.tda367.booleancircuits.view.implementation;
 
+import java.awt.event.ActionListener;
+
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
@@ -12,10 +14,20 @@ import javax.swing.JPopupMenu;
  */
 public final class CanvasPopup extends JPopupMenu {
 	private JMenuItem remove = new JMenuItem("Remove gate");
-	private JMenu connect;
+	private JMenu connect = new JMenu();
 
 	/**
-	 * Creates a pop-up menu with either inputs or outputs.
+	 * Creates a pop-up menu.
+	 */
+	public CanvasPopup() {
+		super();
+
+		add(connect);
+		add(remove);
+	}
+
+	/**
+	 * Updates the pop-up menu with either inputs or outputs.
 	 * 
 	 * @param ports
 	 *            int number of inputs or outputs
@@ -23,17 +35,18 @@ public final class CanvasPopup extends JPopupMenu {
 	 *            boolean true if the menu is an input menu, false if output
 	 *            menu
 	 */
-	public CanvasPopup(int ports, boolean isInput) {
-		super();
+	public void updateMenu(int ports, boolean isInput) {
 		if (isInput) {
-			connect = new JMenu("Connect input...");
+			connect.setText("Connect input...");
 		} else {
-			connect = new JMenu("Connect output...");
+			connect.setText("Connect output...");
 		}
+		connect.removeAll();
+
 		for (int i = 1; i <= ports; i++) {
 			connect.add(new JMenuItem("" + i));
 		}
-		add(connect);
-		add(remove);
+		
+		connect.setEnabled(ports > 0);
 	}
 }
