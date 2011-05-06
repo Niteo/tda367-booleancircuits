0d507c8883713c11e125c3f653d4933549daaf33
niteo1337@gmail.com <niteo1337@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-05-06 10:35:53 +0000

    Pop-up menu grayed out if the component doesn't have inputs or outputs.

--- BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/implementation/Canvas.java
+++ BooleanCircuits/src/edu/chl/tda367/booleancircuits/view/implementation/Canvas.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.beans.PropertyChangeListener;
@@ -32,18 +34,101 @@ import edu.chl.tda367.booleancircuits.view.draw.implementation.Draw;
  */
 public class Canvas {
 
-	private JPanel panel;
 	private IModel model;
 	private ISelectionModel selectModel;
-	private MouseAdapter mouseAdapter;
 	private static IDraw drawer = new Draw();
 	private int posX, posY;
 	private int zoomFactor;
 	private Point oldDragPosition;
 	private boolean input;
 	private MasterController mc;
+	private CanvasPopup menu = new CanvasPopup();
+	private ActionListener listener = new ActionListener() {
 
-	public Canvas(IModel canvasModel, ISelectionModel selectionModel, MasterController masterController) {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+	};
+
+	private JPanel panel = new JPanel() {
+		@Override
+		public void paint(Graphics g) {
+			Graphics2D g2d = (Graphics2D) g;
+			super.paint(g2d);
+			// Draw background
+			drawer.drawBackground(g2d, new Point(posX, posY), panel.getSize());
+			// Draw components
+			if (model != null) {
+				for (IAbstractCircuitGate circuitGate : model.getComponents()) {
+					// Set color
+					if (selectModel.isSelectedComponent(circuitGate)) {
+						g2d.setColor(Color.BLUE);
+					} else {
+						g2d.setColor(Color.BLACK);
+					}
+					// Draw component
+					drawer.drawGate(g2d, circuitGate, new Point(posX, posY));
+				}
+			}
+			// Draw position
+			g2d.setColor(Color.BLACK);
+			g2d.drawString("[" + posX + ", " + posY + "]", 5, 15);
+		}
+	};
+
+	private MouseAdapter mouseAdapter = new MouseInputAdapter() {
+
+		@Override
+		public void mouseDragged(MouseEvent evt) {
+			if (oldDragPosition != null) {
+				int dx = (int) (evt.getPoint().getX() - oldDragPosition.getX());
+				int dy = (int) (evt.getPoint().getY() - oldDragPosition.getY());
+				panCanvas(-dx, -dy);
+			}
+			oldDragPosition = evt.getPoint();
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent evt) {
+			oldDragPosition = null;
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent evt) {
+			Point pointClicked = new Point(evt.getX() + posX, evt.getY() + posY);
+
+			if (evt.getButton() == MouseEvent.BUTTON1) { // LMB
+				if (evt.getClickCount() == 1) {
+					mc.selectComponent(pointClicked);
+					input = true;
+				} else if (evt.getClickCount() == 2) {
+					mc.addComponent(pointClicked);
+				}
+			} else if (evt.getButton() == MouseEvent.BUTTON3) { // RMB
+
+				IAbstractCircuitGate gate = model.getComponent(pointClicked);
+				JPopupMenu jpm = new JPopupMenu();
+				
+				if (gate == null) {
+					jpm.add(new JMenuItem("<No components>"));
+				} else {
+					jpm = menu;
+					menu.updateMenu(
+							input ? gate.getNoOfInputs() : gate
+									.getNoOfOutputs(), input);
+				}
+
+				jpm.show(evt.getComponent(), (int) evt.getPoint().getX(),
+						(int) evt.getPoint().getY());
+			}
+		}
+	};
+
+	public Canvas(IModel canvasModel, ISelectionModel selectionModel,
+			MasterController masterController) {
 		mc = masterController;
 		input = true;
 		posX = 0;
@@ -51,89 +136,7 @@ public class Canvas {
 		zoomFactor = 0;
 		model = canvasModel;
 		selectModel = selectionModel;
-		mouseAdapter = new MouseInputAdapter() {
 
-			@Override
-			public void mouseDragged(MouseEvent evt) {
-				if (oldDragPosition != null) {
-					int dx = (int) (evt.getPoint().getX() - oldDragPosition
-							.getX());
-					int dy = (int) (evt.getPoint().getY() - oldDragPosition
-							.getY());
-					panCanvas(-dx, -dy);
-				}
-				oldDragPosition = evt.getPoint();
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent evt) {
-				oldDragPosition = null;
-			}
-
-			@Override
-			public void mouseClicked(MouseEvent evt) {
-				Point pointClicked = new Point(evt.getX() + posX, evt.getY()
-						+ posY);
-
-				if (evt.getButton() == MouseEvent.BUTTON1) { // LMB
-					if (evt.getClickCount() == 1) {
-						mc.selectComponent(pointClicked);
-						input = true;
-					} else if (evt.getClickCount() == 2) {
-						mc.addComponent(pointClicked);
-					}
-				} else if (evt.getButton() == MouseEvent.BUTTON3) { // RMB
-
-					IAbstractCircuitGate gate = model.getComponent(pointClicked);
-					JPopupMenu jpm = new JPopupMenu();
-
-					if (gate == null) {
-						jpm.add(new JMenuItem("LOLFAG. NO COMPONENTS THERE! >:"));
-					} else {
-						if (input) {
-							jpm = new CanvasPopup(gate.getNoOfInputs(), true);
-						} else {
-							jpm = new CanvasPopup(gate.getNoOfOutputs(), false);
-						}
-
-					}
-
-					jpm.show(evt.getComponent(), (int) evt.getPoint().getX(),
-							(int) evt.getPoint().getY());
-				}
-			}
-		};
-
-		panel = new JPanel() {
-			@Override
-			public void paint(Graphics g) {
-				Graphics2D g2d = (Graphics2D) g;
-				super.paint(g2d);
-				// Draw background
-				drawer.drawBackground(g2d, new Point(posX, posY),
-						panel.getSize());
-				// Draw components
-				if (model != null) {
-					for (IAbstractCircuitGate circuitGate : model
-							.getComponents()) {
-						// Set color
-						if (selectModel.isSelectedComponent(circuitGate)) { // TODO:
-																			// <----FIX
-																			// THIS
-																			// SHIT
-							g2d.setColor(Color.BLUE);
-						} else {
-							g2d.setColor(Color.BLACK);
-						}
-						// Draw component
-						drawer.drawGate(g2d, circuitGate, new Point(posX, posY));
-					}
-				}
-				// Draw position
-				g2d.setColor(Color.BLACK);
-				g2d.drawString("[" + posX + ", " + posY + "]", 5, 15);
-			}
-		};
 		panel.setBackground(Color.WHITE);
 		panel.addMouseListener(mouseAdapter);
 		panel.addMouseMotionListener(mouseAdapter);
