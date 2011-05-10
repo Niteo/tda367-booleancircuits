2f71c26e25e9f2ccd04caf20706b32e96dcf31e0
niteo1337@gmail.com <niteo1337@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-05-10 11:22:11 +0000

    Main method moved from MainWindow to a Main class.

---
+++ trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/Main.java
@@ -0,0 +1,20 @@
+package edu.chl.tda367.booleancircuits;
+
+import edu.chl.tda367.booleancircuits.view.implementation.MainWindow;
+
+/**
+ * The main class.
+ * 
+ * @author Boel
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String args[]) {
+		new MainWindow().setVisible(true);
+	}
+}
