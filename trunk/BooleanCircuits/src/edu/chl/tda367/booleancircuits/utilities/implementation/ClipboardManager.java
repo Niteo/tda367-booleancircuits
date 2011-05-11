cc86e0344fe1c2d927c2d76219887a2f5e963a9e
anton.lin1@gmail.com <anton.lin1@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-05-11 12:43:33 +0000

    ClipboardManager not yet done

--- trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/utilities/implementation/ClipboardManager.java
+++ trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/utilities/implementation/ClipboardManager.java
@@ -4,21 +4,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 import edu.chl.tda367.booleancircuits.model.components.IAbstractCircuitGate;
+import edu.chl.tda367.booleancircuits.model.components.IGateInput;
 import edu.chl.tda367.booleancircuits.utilities.IClipboardManager;
 
 public class ClipboardManager implements IClipboardManager {
-	
-	private List<IAbstractCircuitGate> clipboardCollection = new ArrayList<IAbstractCircuitGate>();
+
+	private List<IAbstractCircuitGate> clipboardList = new ArrayList<IAbstractCircuitGate>();
 
 	@Override
-	public void copy(List<IAbstractCircuitGate> gate) {
-		clipboardCollection.clear();
-		clipboardCollection.addAll(gate);
+	public void copy(List<IAbstractCircuitGate> componentsList) {
+		clipboardList.clear();
+
+		/*
+		 * Add gates
+		 */
+		for (IAbstractCircuitGate component : componentsList) {
+			IAbstractCircuitGate gate = component.clone();
+			clipboardList.add(gate);
+		}
+		/*
+		 * Check internal connections
+		 */
+		for (IAbstractCircuitGate component : clipboardList) {
+			for (IGateInput input : component.getInputs()) {
+				if (componentsList.contains(input.getInputComponent())) {
+					component.connectInput(
+							component.getInputs().indexOf(input), clipboardList
+									.get(componentsList.indexOf(input
+											.getInputComponent())), input
+									.getInputPort());
+				} else {
+					System.out.println("Hello");
+					component.connectInput(0, null, -1);
+				}
+			}
+		}
 	}
 
 	@Override
 	public List<IAbstractCircuitGate> paste() {
-		return clipboardCollection;
+		return clipboardList;
 	}
 
 }
