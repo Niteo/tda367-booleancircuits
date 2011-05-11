101c3a8f4934753b05ab2e0983e9d05ffa840b5d
jenpan90@hotmail.com <jenpan90@hotmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-05-11 09:40:28 +0000

    not fully implemented test classes for the component classes fully implemented test class for the Model class

---
+++ trunk/BooleanCircuits/test/edu/chl/tda367/booleancircuits/model/components/implementation/NotGateTest.java
@@ -0,0 +1,103 @@
+package edu.chl.tda367.booleancircuits.model.components.implementation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NotGateTest {
+	
+	@Test
+	public void testNotGate() {
+		NotGate not = new NotGate();
+		assertTrue(not != null && not.getNoOfInputs() == 1);
+	}
+
+	@Test
+	public void testUpdateOutput() {
+		NotGate not = new NotGate();
+	//TODO:more implementation here
+	}
+
+	@Test
+	public void testEmptyGateClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+
+
+	@Test
+	public void testGetInputs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetOutput() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateIO() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOverwriteGate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConnectInput() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOutputValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNoOfInputs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNoOfOutputs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetComponentTier() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+}
