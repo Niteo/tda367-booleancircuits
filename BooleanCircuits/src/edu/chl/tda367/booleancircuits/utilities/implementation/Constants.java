d59123a55917d021da7bc0ca63ba531f0ddb9152
robelirobban@gmail.com <robelirobban@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-05-11 11:15:21 +0000

    CCP right-click menu.

--- BooleanCircuits/src/edu/chl/tda367/booleancircuits/utilities/implementation/Constants.java
+++ BooleanCircuits/src/edu/chl/tda367/booleancircuits/utilities/implementation/Constants.java
@@ -1,9 +1,12 @@
 package edu.chl.tda367.booleancircuits.utilities.implementation;
 
+import java.awt.Color;
+
 public class Constants {
 	public static final int canvasBackgroundLineDistance = 32;
 	public static final int componentSize = 32;
 	public static final int componentCircleRadius = 10;
 	public static final int canvasDotDiameter = 1;
-	
+	public static final Color connectionLow = Color.black;
+	public static final Color connectionHigh = Color.red;
 }
