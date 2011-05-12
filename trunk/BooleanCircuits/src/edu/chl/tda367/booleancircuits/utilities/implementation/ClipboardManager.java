96c9bcdc4a76e7d35231c424d288877c3e7bf574
anton.lin1@gmail.com <anton.lin1@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-05-12 11:02:45 +0000

    Copy and paste function fully implemented.

--- trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/utilities/implementation/ClipboardManager.java
+++ trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/utilities/implementation/ClipboardManager.java
@@ -1,7 +1,9 @@
 package edu.chl.tda367.booleancircuits.utilities.implementation;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import edu.chl.tda367.booleancircuits.model.components.IAbstractCircuitGate;
 import edu.chl.tda367.booleancircuits.model.components.IGateInput;
@@ -9,44 +11,41 @@ import edu.chl.tda367.booleancircuits.utilities.IClipboardManager;
 
 public class ClipboardManager implements IClipboardManager {
 
-	private List<IAbstractCircuitGate> clipboardList = new ArrayList<IAbstractCircuitGate>();
+	private List<IAbstractCircuitGate> clipboardList;
+	private Map<IAbstractCircuitGate, IAbstractCircuitGate> componentsMap = new HashMap<IAbstractCircuitGate, IAbstractCircuitGate>();
 
 	@Override
-	public void copy(List<IAbstractCircuitGate> componentsList) {
-		clipboardList.clear();
-
-		/*
-		 * Add gates
-		 */
-		for (IAbstractCircuitGate component : componentsList) {
-			IAbstractCircuitGate gate = component.clone();
-			clipboardList.add(gate);
-		}
-		/*
-		 * Check internal connections
-		 */
-		for (IAbstractCircuitGate component : clipboardList) {
-			for (IGateInput input : component.getInputs()) {
-				if (componentsList.contains(input.getInputComponent())) {
-					component.connectInput(
-							component.getInputs().indexOf(input), clipboardList
-									.get(componentsList.indexOf(input
-											.getInputComponent())), input
-									.getInputPort());
-				} else {
-					System.out.println("Hello");
-					component.connectInput(0, null, -1);
-				}
-			}
-		}
+	public void copy(List<IAbstractCircuitGate> originalList) {
+		clipboardList = duplicateList(originalList);
 	}
 
 	@Override
 	public List<IAbstractCircuitGate> paste() {
-		List<IAbstractCircuitGate> tempList = new ArrayList<IAbstractCircuitGate>();
-		for(int i=0;i<clipboardList.size();i++){
-			tempList.add(clipboardList.get(i).clone());
+		return duplicateList(clipboardList);
+	}
+
+	private List<IAbstractCircuitGate> duplicateList(
+			List<IAbstractCircuitGate> list) {
+
+		List<IAbstractCircuitGate> dupList = new ArrayList<IAbstractCircuitGate>();
+		for (IAbstractCircuitGate gate : list) {
+			componentsMap.put(gate, gate.clone());
 		}
-		return tempList;
+
+		for (IAbstractCircuitGate gate : list) {
+			for (IGateInput input : gate.getInputs()) {
+				if (!list.contains(input.getInputComponent())) {
+					componentsMap.get(gate).connectInput(
+							gate.getInputs().indexOf(input), null, 0);
+				} else {
+					componentsMap.get(gate).connectInput(
+							gate.getInputs().indexOf(input),
+							componentsMap.get(input.getInputComponent()),
+							input.getInputPort());
+				}
+			}
+			dupList.add(componentsMap.get(gate));
+		}
+		return dupList;
 	}
 }
