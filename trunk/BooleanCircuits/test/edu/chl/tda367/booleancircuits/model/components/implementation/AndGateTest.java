012e2321e8a30129e075bdb9fce2c85bb9ad231b
niteo1337@gmail.com <niteo1337@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-05-12 11:22:49 +0000

    AndTest.

--- trunk/BooleanCircuits/test/edu/chl/tda367/booleancircuits/model/components/implementation/AndGateTest.java
+++ trunk/BooleanCircuits/test/edu/chl/tda367/booleancircuits/model/components/implementation/AndGateTest.java
@@ -2,16 +2,30 @@ package edu.chl.tda367.booleancircuits.model.components.implementation;
 
 import static org.junit.Assert.*;
 
+import java.awt.Point;
 import java.util.List;
 
 import org.junit.Test;
+
+import edu.chl.tda367.booleancircuits.model.components.IAbstractCircuitGate;
 
 public class AndGateTest {
 
 	@Test
 	public void testAndGate() {
+		new AndGate(2);
+	}
+	
+	@Test
+	public void testGetNoOfInputs() {
 		AndGate and = new AndGate(2);
-		assertTrue(and != null && and.getNoOfInputs() == 2);
+		assertTrue(and.getNoOfInputs() == 2);
+	}
+
+	@Test
+	public void testGetNoOfOutputs() {
+		AndGate and = new AndGate(2);
+		assertTrue(and.getNoOfOutputs() == 1);
 	}
 
 	@Test
@@ -24,6 +38,12 @@ public class AndGateTest {
 	public void testGetInputs() {
 		AndGate and = new AndGate(2);
 		assertTrue(and.getInputs() instanceof List);
+	}
+
+	@Test
+	public void testGetOutputValue() {
+		AndGate and = new AndGate(2);;
+		assertFalse(and.getOutputValue(0));
 	}
 
 	@Test
@@ -61,60 +81,75 @@ public class AndGateTest {
 	public void testConnectInput() {
 		AndGate and = new AndGate(2);
 		AndGate test = new AndGate(1);
-		
+
 		and.connectInput(0, test, 0);
-		
+
 		assertEquals(test, and.getInputs().get(0).getInputComponent());
 	}
 
 	@Test
-	public void testGetOutputValue() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetNoOfInputs() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetNoOfOutputs() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testUpdate() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testGetComponentTier() {
-		fail("Not yet implemented");
+		AndGate and = new AndGate(2);
+		assertTrue(and.getComponentTier() == 1);
+		and.connectInput(0, new AndGate(2), 0);
+		assertTrue(and.getComponentTier() == 2);
 	}
 
 	@Test
 	public void testGetPosition() {
-		fail("Not yet implemented");
+		AndGate and = new AndGate(2);
+		assertTrue(and.getPosition().x == 0 && and.getPosition().y == 0);
 	}
 
 	@Test
 	public void testSetPosition() {
-		fail("Not yet implemented");
+		AndGate and = new AndGate(2);
+		assertTrue(and.getPosition().x == 0 && and.getPosition().y == 0);
+		Point p = new Point(8, 8);
+		and.setPosition(p);
+		assertTrue(and.getPosition() == p);
 	}
 
 	@Test
 	public void testMove() {
-		fail("Not yet implemented");
+		AndGate and = new AndGate(2);
+		assertTrue(and.getPosition().x == 0 && and.getPosition().y == 0);
+		and.move(7, 8);
+		assertTrue(and.getPosition().x == 7 && and.getPosition().y == 8);
 	}
 
 	@Test
 	public void testClone() {
-		fail("Not yet implemented");
+		AndGate and = new AndGate(2);
+		IAbstractCircuitGate clone = and.clone();
+		assertTrue(clone instanceof AndGate);
+		assertTrue(clone.getPosition().x == and.getPosition().x
+				&& clone.getPosition().y == and.getPosition().y);
+		assertTrue(clone.getNoOfInputs()==and.getNoOfInputs());
+		assertTrue(clone.getNoOfOutputs()==and.getNoOfOutputs());
+	}
+
+	@Test
+	public void testUpdate() {
+		new AndGate(2).update();
 	}
 
 	@Test
 	public void testUpdateOutput() {
-		fail("Not yet implemented");
+		AndGate and = new AndGate(2);
+		assertFalse(and.getOutputValue(0));
+		ConstantGate oneGate1 = new ConstantGate(true);
+		ConstantGate oneGate2 = new ConstantGate(true);
+
+		and.connectInput(0, oneGate1, 0);
+		oneGate1.update();
+		assertTrue(and.getInputs().get(0).getInputValue());
+		and.connectInput(1, oneGate2, 0);
+		oneGate2.update();
+		assertTrue(and.getInputs().get(1).getInputValue());
+
+		and.update();
+		assertTrue(and.getOutputValue(0));
 	}
 
 }
