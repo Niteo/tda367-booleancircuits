77771180e8021409bd946b3c9f35e7ca82db2830
niteo1337@gmail.com <niteo1337@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-05-17 14:58:59 +0000

    SelectionModelTest almost done.

--- trunk/BooleanCircuits/test/edu/chl/tda367/booleancircuits/model/implementation/SelectionModelTest.java
+++ trunk/BooleanCircuits/test/edu/chl/tda367/booleancircuits/model/implementation/SelectionModelTest.java
@@ -2,38 +2,68 @@ package edu.chl.tda367.booleancircuits.model.implementation;
 
 import static org.junit.Assert.*;
 
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+
+import edu.chl.tda367.booleancircuits.model.components.ICircuitGate;
+import edu.chl.tda367.booleancircuits.model.components.implementation.AndGate;
+import edu.chl.tda367.booleancircuits.model.components.implementation.NandGate;
+import edu.chl.tda367.booleancircuits.model.components.implementation.NotGate;
 
 public class SelectionModelTest {
 
 	@Test
 	public void testSelectionModel() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testSelectComponents() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testSelectComponent() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testIsSelectedComponent() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testRemoveUnusedElements() {
-		fail("Not yet implemented"); // TODO
+		new SelectionModel();
 	}
 
 	@Test
 	public void testGetSelectedComponents() {
-		fail("Not yet implemented"); // TODO
+		SelectionModel sm = new SelectionModel();
+		assertTrue(sm.getSelectedComponents().size() == 0);
 	}
 
+	@Test
+	public void testSelectComponent() {
+		SelectionModel sm = new SelectionModel();
+		assertTrue(sm.getSelectedComponents().size() == 0);
+
+		sm.selectComponent(new AndGate(2), false);
+		assertTrue(sm.getSelectedComponents().size() == 1);
+		assertTrue(sm.getSelectedComponents().get(0) instanceof AndGate);
+	}
+
+	@Test
+	public void testSelectComponents() {
+		SelectionModel sm = new SelectionModel();
+		assertTrue(sm.getSelectedComponents().size() == 0);
+		List<ICircuitGate> list = new ArrayList<ICircuitGate>();
+
+		list.add(new AndGate(2));
+		list.add(new NandGate(2));
+
+		sm.selectComponents(list);
+		assertTrue(sm.getSelectedComponents().size() == 2);
+		assertTrue(sm.getSelectedComponents().get(0) instanceof AndGate);
+		assertTrue(sm.getSelectedComponents().get(1) instanceof NandGate);
+
+	}
+
+	@Test
+	public void testIsSelectedComponent() {
+		SelectionModel sm = new SelectionModel();
+		AndGate and = new AndGate(2);
+
+		assertFalse(sm.isSelectedComponent(and));
+		sm.selectComponent(and, false);
+		assertTrue(sm.isSelectedComponent(and));
+	}
+
+	@Test
+	public void testRemoveUnusedElements() {
+		fail("Implement me!");
+	}
 }
