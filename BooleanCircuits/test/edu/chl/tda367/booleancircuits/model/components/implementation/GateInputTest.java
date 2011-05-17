21ed3d6d06c40a9f5f378299334a45e0f40177e8
jenpan90@hotmail.com <jenpan90@hotmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-05-17 08:45:14 +0000

    almost completed test class for GateInput

--- BooleanCircuits/test/edu/chl/tda367/booleancircuits/model/components/implementation/GateInputTest.java
+++ BooleanCircuits/test/edu/chl/tda367/booleancircuits/model/components/implementation/GateInputTest.java
@@ -8,27 +8,49 @@ public class GateInputTest {
 
 	@Test
 	public void testGateInput() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testGetInputComponent() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testGetInputPort() {
-		fail("Not yet implemented"); // TODO
+		new GateInput();
 	}
 
 	@Test
 	public void testSetInputComponent() {
-		fail("Not yet implemented"); // TODO
+		GateInput input = new GateInput();
+		input.setInputComponent(new ConstantGate(false), 0);
+
+	}
+
+	@Test
+	public void testGetInputComponent() {
+		GateInput input = new GateInput();
+		assertNull(input.getInputComponent() );
+
+		input.setInputComponent(new ConstantGate(false), 0);
+		assertTrue(input.getInputComponent() instanceof ConstantGate);
+		
+		
+	}
+
+	@Test
+	public void testGetInputPort() {
+		GateInput input1 = new GateInput();
+		GateInput input2 = new GateInput();
+		assertTrue(input2.getInputPort() == 0);
+
+		input1.setInputComponent(new ConstantGate(false), 0);
+		input2.setInputComponent(new AndGate(2), 2);
+
+		assertTrue(input1.getInputPort() == 0);
+		assertTrue(input2.getInputPort() == 2);
+
 	}
 
 	@Test
 	public void testGetInputValue() {
-		fail("Not yet implemented"); // TODO
+		GateInput input1 = new GateInput();
+		GateInput input2 = new GateInput();
+		
+		input2.setInputComponent(new ConstantGate(true), 0);
+		assertTrue(input1.getInputValue()==false);		
+		assertTrue(input2.getInputValue()==true);
 	}
 
 }
