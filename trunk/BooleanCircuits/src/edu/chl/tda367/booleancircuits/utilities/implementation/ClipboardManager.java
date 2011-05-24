2fc079da59a0da0c9a3d7f15ea5d132ad5d11f84
anton.lin1@gmail.com <anton.lin1@gmail.com@bff5b041-b094-7afb-5b5c-16910c1f55ec>
2011-05-24 09:25:27 +0000

    made some changes in clipBoardManager since we made a IGateWrapper class.

--- trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/utilities/implementation/ClipboardManager.java
+++ trunk/BooleanCircuits/src/edu/chl/tda367/booleancircuits/utilities/implementation/ClipboardManager.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import edu.chl.tda367.booleancircuits.model.components.ICircuitGate;
 import edu.chl.tda367.booleancircuits.model.components.IGateInput;
 import edu.chl.tda367.booleancircuits.model.components.IGateWrapper;
 import edu.chl.tda367.booleancircuits.model.components.implementation.GateWrapper;
@@ -12,15 +13,15 @@ import edu.chl.tda367.booleancircuits.utilities.IClipboardManager;
 
 /**
  * handles copying and pasting components
- *
+ * 
  * @author antonlin
- *
+ * 
  */
 
 public class ClipboardManager implements IClipboardManager {
 
 	private List<IGateWrapper> clipboardList = new ArrayList<IGateWrapper>();
-	private Map<IGateWrapper, IGateWrapper> componentsMap = new HashMap<IGateWrapper, IGateWrapper>();
+	private Map<ICircuitGate, IGateWrapper> componentsMap = new HashMap<ICircuitGate, IGateWrapper>();
 	private List<IGateWrapper> lastPastedComponents = new ArrayList<IGateWrapper>();
 
 	@Override
@@ -42,28 +43,31 @@ public class ClipboardManager implements IClipboardManager {
 	private List<IGateWrapper> duplicateList(final List<IGateWrapper> list) {
 
 		List<IGateWrapper> dupList = new ArrayList<IGateWrapper>();
+		List<ICircuitGate> tempList = new ArrayList<ICircuitGate>();
+
 		for (IGateWrapper gate : list) {
 			if (gate != null) {
 				GateWrapper temp = new GateWrapper(gate.getGateClone());
 				temp.setPosition(gate.getPosition());
-				componentsMap.put(gate, temp);
+				componentsMap.put(gate.getGate(), temp);
+				tempList.add(gate.getGate());
 			}
 		}
 
 		for (IGateWrapper gate : list) {
 			if (gate != null) {
 				for (IGateInput input : gate.getInputs()) {
-					if (!list.contains(input.getInputComponent())) {
-						componentsMap.get(gate).connectInput(
+					if (!tempList.contains(input.getInputComponent())) {
+						componentsMap.get(gate.getGate()).connectInput(
 								gate.getInputs().indexOf(input), null, 0);
 					} else {
-						componentsMap.get(gate).connectInput(
+						componentsMap.get(gate.getGate()).connectInput(
 								gate.getInputs().indexOf(input),
-								componentsMap.get(input.getInputComponent()).getGate(),
-								input.getInputPort());
+								componentsMap.get(input.getInputComponent())
+										.getGate(), input.getInputPort());
 					}
 				}
-				dupList.add(componentsMap.get(gate));
+				dupList.add(componentsMap.get(gate.getGate()));
 			}
 		}
 		lastPastedComponents = dupList;
